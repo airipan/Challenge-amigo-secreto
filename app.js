abe9c8d76e8eccca39d5dab01b3b32171fec7213
airipan <120055588+airipan@users.noreply.github.com>
2025-03-13 16:28:01 -0300

    function mostrarAmigos

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ let amigos = [];
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById('nombreAmigo');
-    let nombre = inputAmigo.value.trim(); // Eliminar espacios en blanco
+    let nombre = inputAmigo.value.trim(); // Elimina espacios en blanco
 
     if (nombre === '') {
         alert('Por favor, inserte un nombre.');
         return;
     }
-
     amigos.push(nombre);
-    console.log(amigos); // Para verificar en la consola que se agregó correctamente
 
-    inputAmigo.value = ''; // Limpiar el campo de entrada
+    inputAmigo.value = ''; // Limpia el campo de entrada
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // Limpiar la lista antes de actualizar
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
 }
